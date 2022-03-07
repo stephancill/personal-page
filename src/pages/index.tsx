17b60c0fd5c1c231041f5f88df7ce34586b319db
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2022-03-07 23:32:54 +0200

    Add metadata and icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,15 @@ import "./index.css"
 import { Header } from "../components/Header/Header"
 import { List } from "../components/List/List"
 import items from "./../items.json"
+import { Helmet } from "react-helmet"
 
 const IndexPage = () => {
   return (
     <div style={{display: "flex"}}>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Stephan</title>
+      </Helmet>
       <div style={{marginLeft: "auto", marginRight: "auto", maxWidth: "390px"}}>
         <div style={{marginTop: "120px", marginBottom: "120px"}}>
           <Header/>
