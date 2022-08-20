eb66c1500e6f9f30477df7bbd891c2af8ec17e62
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2022-08-20 16:17:13 +0200

    Add personal avatar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,12 +3,13 @@ import * as style from "./Header.module.css"
 import TwitterIcon from "../../images/twitter.svg"
 import GithubIcon from "../../images/github.svg"
 import LinkedinIcon from "../../images/linkedin.svg"
+import { StaticImage } from "gatsby-plugin-image"
 
 export const Header = () => {
   return (
     <div style={{ marginBottom: "35px" }}>
       <div style={{ display: "flex" }}>
-        <img src="https://metadata.ens.domains/mainnet/avatar/stephancill.eth" className={style.avatar}></img>
+        <StaticImage src="../../images/avatar.jpg" className={style.avatar} alt="avatar" />
         <div style={{ marginLeft: "10px" }}>
           <div className={`${style.line}`} style={{ fontSize: "24px" }}>
             Stephan Cilliers
